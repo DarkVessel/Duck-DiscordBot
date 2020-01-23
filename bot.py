a67471c3f2b3a9e72bbb53aa7a4ba699c1adcac5
DarkVessel <rs5334819@gmail.com>
2020-01-23 22:55:07 +0400

    Тест коммит.

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
 import config
+from colors import out_green
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print("Start bot!")
+        out_green("{} вышел на связь!".format(self.user.name))
 
 
 client = MyClient()
---
+++ test.py
@@ -0,0 +1,3 @@
+text = "Какой-то текст."
+print("\033[30m {}".format(text)) #
+print("\033[31m {}".format(text))
